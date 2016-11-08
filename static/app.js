88321cdfe2b9b6b8109fcc734860e6a804c6930e
David Figueroa <diego.david.figueroa@gmail.com>
2016-11-08 00:44:04 -0300

    Formatting + Ads + Sort

--- static/app.js
+++ static/app.js
@@ -4,3 +4,15 @@ const app = angular.module('asciiApp', [
   'angularGrid',
   'APIService'
 ]);
+
+app.constant('constants', {
+  pageSize: 20,
+  defaultSort: 'id',
+  modAds: 16,
+});
+
+app.filter('date', () => (dateString) => {
+  const theMoment = moment(new Date(dateString));
+  const isLessThanAWeekBeforeNow = moment().subtract(7, 'days').isBefore(theMoment);
+  return isLessThanAWeekBeforeNow ? theMoment.fromNow() : theMoment.format();
+});
--- static/scripts/controllers/gridController.js
+++ static/scripts/controllers/gridController.js
@@ -1,11 +1,14 @@
 'use strict';
 
-angular.module('asciiApp').controller('gridController', ['$scope', 'productsService',
-  function ($scope, productsService) {
+angular.module('asciiApp').controller('gridController', ['$scope', 'productsService', 'constants',
+  function ($scope, productsService, constants) {
     $scope.products;
     let products;
-    const pageSize = 20;
+    const pageSize = constants.pageSize;
+    const modAds = constants.modAds;
+    let currentSort = constants.defaultSort;
     let currentPage = 1;
+    let lastRandom = 0;
     $scope.end = false;
 
     productsService.getProducts({}).$promise.then(response => {
@@ -13,11 +16,32 @@ angular.module('asciiApp').controller('gridController', ['$scope', 'productsServ
       $scope.products = products.concat([]);
     });
 
+    /* So ads don't get repeated perform same processing as the backend */
+    const validRandom = (number) => (number % modAds) !== (lastRandom % modAds);
+
+    const generateRandom = () => {
+      const random = Math.floor(Math.random()*1000);
+      if (!validRandom(random)) return generateRandom();
+      lastRandom = random;
+      return random;
+    };
+
+    const generateAd = () => ({
+      ad: true,
+      random: generateRandom(),
+    });
+
     $scope.loadMore = () => {
+      if ($scope.end) return;
       productsService.getProducts({
         skip: pageSize * currentPage,
+        sort: currentSort,
       }).$promise.then(nextPageData => {
-        if (!nextPageData.length) $scope.end = true;
+        if (!nextPageData.length) {
+          $scope.end = true;
+          return;
+        }
+        $scope.products.push(generateAd());
         $scope.products = $scope.products.concat(nextPageData);
         currentPage++;
       });
@@ -27,8 +51,10 @@ angular.module('asciiApp').controller('gridController', ['$scope', 'productsServ
       productsService.getProducts({
         sort: field,
       }).$promise.then(response => {
+        currentSort = field;
         $scope.products = response;
         currentPage = 1;
+        $scope.end = false;
       });
     };
   }
